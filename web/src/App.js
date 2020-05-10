92d53cdb182ccdebc74f0c7d58a42dbf7aab3db6
Felipe Carvalho <felipe.carvalho07@gmail.com>
2020-05-10 13:52:31 -0300

    starting front-end

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import ForumNavbar from './components/Navbars/Navbar';
+// import { Router } from 'react-router';
+import Home from "./views/Home.js";
+// import Index from "./views/Index.js";
+import Landing from "./views/examples/Landing.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <ForumNavbar />
+        <Switch>
+          <Route path="/" exact render={props => <Home {...props} />} />
+          <Route
+            path="/landing-page"
+            exact
+            render={props => <Landing {...props} />}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
